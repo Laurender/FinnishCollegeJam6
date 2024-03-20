fe5d9ccaad632d8ca37265ecf5fb502b619e8953
Lauri Leponiemi <laurileponiemi@gmail.com>
2024-03-20 14:57:50 +0200

    To level after cutscene

--- FCJ6/Assets/Scripts/Cutscene.cs
+++ FCJ6/Assets/Scripts/Cutscene.cs
@@ -8,7 +8,7 @@ public class Cutscene : MonoBehaviour
 {
     string boy1 = "Grandson: Ugh. It's so annoying, it's so hard to wake up on time and I'm always late to school.";
     string grandpa1 = "Grandpa: Bah! Back in my day, we were late to school even if we woke up two hours too early!";
-    public string nextScene;
+    public int nextScene;
     public TMP_Text subtitle;
     // Start is called before the first frame update
     void Start()
