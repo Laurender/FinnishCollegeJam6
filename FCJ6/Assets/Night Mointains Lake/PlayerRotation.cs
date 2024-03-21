e14cb08d7a4f16a28d9ac81e473e295b6bf6c673
Lauri Leponiemi <laurileponiemi@gmail.com>
2024-03-21 13:13:22 +0200

    Hahmo loikkii

--- FCJ6/Assets/Night Mointains Lake/PlayerRotation.cs
+++ FCJ6/Assets/Night Mointains Lake/PlayerRotation.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
+    Vector3 myScale;
+    Vector3 pushScale;
+    bool canPush = true;
+    public float speed = 50f;
     void Start()
     {
-        
+        myScale = transform.localScale;
+        pushScale = myScale * 1.3f;
     }
 
     void Update()
     {
-        Vector3 mousePosition = Input.mousePosition;
+        /*Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
-        transform.up = direction.normalized;
+        transform.up = direction.normalized;*/
+
+        transform.Rotate(0f, 0f, -Input.GetAxis("Horizontal")*speed);
+
+        if (Input.GetMouseButtonDown(0) && canPush)
+        {
+            StartCoroutine(PushHands());
+        }
+    }
+
+    IEnumerator PushHands (){
+        canPush = false;
+        transform.localScale = pushScale;
+        yield return new WaitForSeconds(Time.deltaTime * 2);
+        transform.localScale = myScale;
+        canPush = true;
     }
 }
---
+++ FCJ6/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float timer = 0f;
+    bool jumpReady = true;
+    bool isGrounded = true;
+    public float force = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0f && !jumpReady)
+        {
+            timer -= Time.deltaTime;
+        } else if (!jumpReady)
+        {
+            jumpReady = true;
+        }
+
+        bool lmb = Input.GetMouseButtonDown(0);
+        if(lmb && isGrounded && jumpReady)
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+            Vector3 objPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            Vector3 dir = objPos - this.transform.position;
+
+            this.GetComponentInParent<Rigidbody2D>().AddForce(dir.normalized * force, ForceMode2D.Impulse);
+
+            Debug.Log("AddForce");
+
+            jumpReady = false;
+            timer = 0.5f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
